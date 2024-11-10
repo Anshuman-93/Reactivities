d4e7c5101a7d82139d196aadd6a2adaacc2de299
Anshuman Rout <anshuman.rout@aveva.com>
2024-11-10 06:12:54 +0530

    added agent and loader functionality

--- client-app/src/features/activities/dasboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dasboard/ActivityDashboard.tsx
@@ -13,7 +13,8 @@ interface Props {
   openForm: (id: string) => void;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
-  deleteActivity:(id:string)=>void
+  deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 export default function ActivityDashboard({
   activities,
@@ -24,12 +25,18 @@ export default function ActivityDashboard({
   openForm,
   closeForm,
   createOrEdit,
-  deleteActivity
+  deleteActivity,
+  submitting,
 }: Props) {
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={ deleteActivity} />
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+          submitting={submitting}
+        />
       </Grid.Column>
       <Grid.Column width="6">
         {selectedActivity && !editMode && (
@@ -44,6 +51,7 @@ export default function ActivityDashboard({
             closeForm={closeForm}
             activity={selectedActivity}
             createOrEdit={createOrEdit}
+            submitting={submitting}
           />
         )}
       </Grid.Column>
